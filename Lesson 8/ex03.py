a310a61f9483e72acc027f9afa0e4b2d50475633
kovasa <60367138+kovasa@users.noreply.github.com>
2020-02-22 15:10:37 +0300

    Add files via upload

--- Lesson 8/ex03.py
+++ Lesson 8/ex03.py
@@ -6,28 +6,37 @@
     В ответе будет получен файл изображения jpeg, в виде octet-stream,
     который нужно раскодировать и сохранить на компьютере под целевым
     именем, переданным в аргументе.
-    Функция должна вернуть размер сохраненного файла."""
+    Функция должна вернуть размер сохраненного файла.
+"""
 
 
+import base64
+import os
 import requests
 
 
-def save_jpeg(url: str, name: str):
-    """Function  that sends an HTTP file and the response
-       file saves and returns its size. """
-
+def save_jpeg(path_in: str, name_out: str):
+    """Sends HTTP POST request to https://postman-echo.com/postfile,
+       saves octet-stream response into JPEG file and
+       returns its size.
+    """
     url_post = 'https://postman-echo.com/post'
-    files = {'photo': open(url, 'rb')}
-    req = requests.post(url_post, files=files)
-    res = req.content
+    files = {'photo': open(path_in, 'rb')}
 
-    filename = f'E:\{name}'
-    with open(filename, 'wb') as f:
-        f.write(res)
+    req = requests.post(url_post, files=files)
+    dir_in, name_in = os.path.split(path_in)
+    res = req.json()['files'][name_in].split(',')[-1]
+
+    filename = os.path.join(dir_in, name_out)
+    with open(filename, 'wb') as f_out:
+        code = base64.decodebytes(res.encode('utf-8'))
+        f_out.write(code)
+
+    return code.__sizeof__()
 
 
 def _test():
-    save_jpeg('E:\orig.jpeg', 'test.jpeg')
+    print(save_jpeg('E:\\orig.jpeg', 'test.jpeg'))
 
 
 _test()
